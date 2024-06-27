9c3da965b4c537cce753c2bc1132762fd8e77dad
Tudor Bungardean <tudorionutbungardean@gmail.com>
2024-06-27 16:07:07 +0300

    create s3 connection

--- app/router/api.py
+++ app/router/api.py
@@ -2,8 +2,15 @@ from fastapi import APIRouter
 
 router = APIRouter()
 
-@router.get("")
-async def test():
-    return {
-        "message":"Hello world"
-    }
+# set the name of your bucket
+BUCKET = "keystone-specs3"
+
+s3 = boto3.client('s3')
+
+
+@router.get("/specs")
+async def get_user_library(user: str):
+    response = s3.list_objects_v2(Bucket=BUCKET, Prefix=f"{user}/specs/")
+
+    return [obj['Key'].split("/specs/")[-1] for obj in response.get('Contents', [])]
+
