6601f63aa92dd644e34d9ee1e0f1ca8d74a63654
Tudor Bungardean <tudorionutbungardean@gmail.com>
2024-09-01 04:41:19 +0300

    remove uvicorn run

--- app/main.py
+++ app/main.py
@@ -19,5 +19,5 @@ app.include_router(router, prefix="/api")
 handler = Mangum(app)
 
 
-if __name__ == "__main__":
-    uvicorn.run(app, port=8000)
+# if __name__ == "__main__":
+#     uvicorn.run(app, port=8000)
